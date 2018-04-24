5690300b67317c0989af7cbef80f2107f194a472
Vasilis Mourtzis <vasilis.mourtzis@outlook.com>
2018-04-24 17:53:28 +1000

    Moved the client form schema to the client constants

--- src/components/Client/add.vue
+++ src/components/Client/add.vue
@@ -23,9 +23,8 @@
 </template>
 
 <script>
-import {requiredRule, emailRule} from "../../constants/rules";
 import formGenerator from "../shared/formGenerator";
-import {SINGLE_LINE_TEXT, SPACER, SUBHEADING, LIST} from "../../constants/formGenerator";
+import {clientSchema} from "../../constants/client";
 
 export default {
   components: {
@@ -46,24 +45,7 @@ export default {
         primaryPhone: "",
         contacts: []
       },
-      schema: [
-        {type: SINGLE_LINE_TEXT, name: "businessName", label: "Business Name", rules: [requiredRule], largeQuarter: true, required: true, },
-        {type: SINGLE_LINE_TEXT, name: "invoicePrefix", label: "Invoice Prefix", rules:[requiredRule], smallQuarter: true, required: true, counter: 4, mask: "AAAA"},
-        {type: SINGLE_LINE_TEXT, name: "address", label: "Addresss", rules: [requiredRule], full: true, required: true},
-        {type: SPACER},
-        {type: SUBHEADING, label: "Contact Details"},
-        {type: SINGLE_LINE_TEXT, name: "firstname", label: "First Name", rules: [requiredRule], half: true, required: true},
-        {type: SINGLE_LINE_TEXT, name: "lastname", label: "Last Name", rules: [requiredRule], half: true, required: true},
-        {type: SINGLE_LINE_TEXT, name: "email", fieldType:"email", label: "Email", rules: [requiredRule, emailRule], half: true, required: true},
-        {type: SINGLE_LINE_TEXT, name: "primaryPhone", label: "Primary Phone", rules: [requiredRule], half: true, required: true, mask: "phone"},
-        {type: SPACER},
-        {type: SUBHEADING, label: "Additional Contacts"},
-        {type: LIST, name:"contacts", addBtnName:"New Phone", fields: [
-          {type: SINGLE_LINE_TEXT, name: "name", label: "Contact Name", rules: [requiredRule], small: true, required: true},
-          {type: SINGLE_LINE_TEXT, name: "phone", label: "Phone", rules: [requiredRule], large: true, required: true, mask: "phone"},
-        ]
-        }
-      ]
+      schema: clientSchema
     };
   },
   methods: {
